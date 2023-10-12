fcb35e40c9d9ead6fbc5e3d1cd83bca6b20461e6
Mandy Wehrman <arwehrman@gmail.com>
2023-10-12 14:55:05 -0400

    feat(Badge): adds commented out code for withPadding not working in View

--- packages/solid/components/Badge/Badge.jsx
+++ packages/solid/components/Badge/Badge.jsx
@@ -1,13 +1,13 @@
 import { View, Text } from '@lightningjs/solid';
 import styles from './Badge.styles';
-import { withPadding } from '@lightningjs/solid-primitives';
+//import { withPadding } from '@lightningjs/solid-primitives';
 
 // TODO: Add Icon component & padding
 
 const Badge = props => {
   return (
     <View
-      use:withPadding={[7, 15, 13, 15]}
+      //use:withPadding={[7, 15, 13, 15]}
       {...props}
       style={styles.container}
       color={props.color}
