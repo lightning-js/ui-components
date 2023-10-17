200beab7eae4b15c978410a98e824bdab0877a9e
Anthony Gallo <anthony_gallo2@comcast.com>
2023-10-17 11:53:41 -0400

    fix: use solid/vite.config for storybook builder configuration

--- src/packages/solid/.storybook/main.ts
+++ src/packages/solid/.storybook/main.ts
@@ -1,7 +1,5 @@
 /** @type { import('storybook-solidjs-vite').StorybookConfig } */
 
-import { mergeConfig } from 'vite';
-
 const config = {
   stories: ['../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
@@ -11,23 +9,6 @@ const config = {
   },
   docs: {
     autodocs: 'tag'
-  },
-  staticDirs: ['../../../shared/public'],
-  viteFinal: async (config, { configType }) => {
-    // console.log('PREVIEW VITE');
-    // if (configType === 'PRODUCTION') {
-    //   mergeConfig(config, {
-    //     base: ''
-    //     // base: process.cwd(),
-    //     // publicDir: '../../shared/public'
-    //   });
-    // }
-    // config.base = process.cwd();
-    // config.base = '';
-    // config.root = '';
-    // config.server.fsServe = undefined;
-    // console.log(config.base);
-    return config;
-  },
+  }
 };
 export default config;
--- src/packages/solid/vite.config.js
+++ src/packages/solid/vite.config.js
@@ -1,7 +1,53 @@
 import { defineConfig } from 'vite';
-import config from '../../../vite.config';
+import solidPlugin from 'vite-plugin-solid';
+import { importChunkUrl } from '@lightningjs/vite-plugin-import-chunk-url';
 
-export default defineConfig({
-  ...config,
-  publicDir: '../../shared/public'
-});
+const config = {
+  optimizeDeps: {
+    include: [],
+    exclude: [
+      '@lightningjs/solid',
+      '@lightningjs/solid-primitives',
+      '@lightningjs/renderer/core',
+      '@lightningjs/renderer/workers/renderer'
+    ]
+  },
+  plugins: [
+    solidPlugin({
+      solid: {
+        moduleName: '@lightningjs/solid',
+        generate: 'universal'
+      }
+    })
+  ],
+  base: '',
+  resolve: {
+    alias: {
+      theme: '@lightningjs/ui-components-theme-base'
+    },
+    dedupe: ['solid-js', '@lightningjs/solid']
+  },
+  server: {
+    hmr: false,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp'
+    }
+  }
+};
+
+export default ({ mode }) => {
+  /**
+   * mode
+   * development - storybook dev server
+   * production - storybook static site build
+   */
+
+  if (mode === 'development') {
+    // TODO font loading only works in dev because the static site builder doesn't work with the `importChunkUrl` plugin
+    config.plugins = [...config.plugins, importChunkUrl()];
+    config.publicDir = '../../shared/public';
+  }
+
+  return defineConfig(config);
+};
