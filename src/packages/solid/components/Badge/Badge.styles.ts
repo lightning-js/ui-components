c80b4da466129f882c15239421d20812fcf15e77
Mandy Wehrman <arwehrman@gmail.com>
2023-10-20 13:55:20 -0400

    feat(Badge): adds Icon to Badge
    feat(Badge): adds icon component and icon props
    
    feat(Badge): adds commented notes
    
    fix(Badge): adds license to badge files
    
    fix(Badge): updates missed rebase

--- src/packages/solid/components/Badge/Badge.styles.ts
+++ src/packages/solid/components/Badge/Badge.styles.ts
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import theme from 'theme';
 import { getHexColor } from '../../../../shared/utils/index'; // TODO ts path aliasing
 
@@ -14,15 +31,16 @@ import { getHexColor } from '../../../../shared/utils/index'; // TODO ts path al
 //    }
 
 type BadgeStyle = {
-  container: {
+  width: number;
+  height: number;
+  color: number;
+  padding: number[];
+  paddingX: number;
+  paddingY: number;
+  borderRadius: number;
+  border: {
     width: number;
-    height: number;
     color: number;
-    borderRadius: number;
-    border: {
-      width: number;
-      color: number;
-    };
   };
   textStyle: {
     width: number;
@@ -32,18 +50,19 @@ type BadgeStyle = {
 };
 
 const styles: BadgeStyle = {
-  container: {
-    width: 200,
-    height: theme.typography.tag1.lineHeight,
-    color: getHexColor(...(theme.color.fillBrand as [string, number])),
-    borderRadius: theme.radius.sm,
-    border: {
-      width: theme.stroke.sm,
-      color: getHexColor(...(theme.color.strokeInverse as [string, number]))
-    }
+  width: 300,
+  height: theme.typography.tag1.lineHeight,
+  paddingX: theme.spacer.md + theme.spacer.xxs,
+  paddingY: theme.spacer.xs,
+  padding: [theme.spacer.md + theme.spacer.xxs, theme.spacer.xs],
+  color: getHexColor(...(theme.color.fillBrand as [string, number])),
+  borderRadius: theme.radius.sm,
+  border: {
+    width: theme.stroke.sm,
+    color: getHexColor(...(theme.color.strokeInverse as [string, number]))
   },
   textStyle: {
-    width: 200,
+    width: 300,
     ...theme.typography.tag1,
     textAlign: 'center'
   }
--- src/packages/solid/components/Badge/Badge.tsx
+++ src/packages/solid/components/Badge/Badge.tsx
@@ -1,26 +1,92 @@
+/*
+ * Copyright 2023 Comcast Cable Communications Management, LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 import type { Component } from 'solid-js';
-import { Text, View } from '@lightningjs/solid';
-import styles from './Badge.styles';
+import { Text, Show } from '@lightningjs/solid';
 import { withPadding } from '@lightningjs/solid-primitives';
+import Icon, { type IconProps } from '../Icon/Icon';
+import styles from './Badge.styles';
+
 withPadding; // Preserve the import.
 
-// TODO: Add Icon component
 // props in LUI: title, icon, iconAlign, iconWidth, iconHeight
+// NOTE: Badge can have an icon on left or right or no Icon
 
 type BadgeProps = {
   /**
    * Badge text
    */
   title: string;
+  /**
+   * side of the text where icon will appear on
+   */
+  iconAlign: string;
+  /**
+   * width of icon
+   */
+  iconWidth: number;
+  /**
+   * height of icon
+   */
+  iconHeight: number;
+  /**
+   * Object containing all properties supported in the [Icon component](?path=/docs/components-icon--icon)
+   */
+  icon: Partial<IconProps>;
 };
 
-// use <Show> to conditionally show icons &/or text?
-
 const Badge: Component<BadgeProps> = (props: BadgeProps) => {
+  // not implemented, thinking about which option would be best
+  const iconAlignStyles = (props) => {
+    if (props.iconAlign) {
+      if (props.iconAlign === 'left') {
+        return {
+          // return styles to align icon on left of text
+        };
+      } else {
+        // return styles to align icon on the right of text
+      }
+    }
+  };
+
   // NOTE: directive needs to be before props
   return (
-    <node use:withPadding={[20, 30]} {...props} style={styles.container}>
-      <Text style={styles.textStyle}>{props.title}</Text>
+    <node use:withPadding={[10, 10]} {...props} style={styles}>
+      <Show when={props.iconAlign === 'left'}>
+        <Icon
+          color={props.icon.color}
+          icon={props.icon.icon}
+          width={props.iconWidth}
+          height={props.iconHeight}
+        />
+      </Show>
+      <Text x={props.iconWidth + 10} y={styles.paddingY + 10} style={styles.textStyle}>
+        {props.title}
+      </Text>
+      <Show when={props.iconAlign === 'right'}>
+        <Icon
+          color={props.icon.color}
+          icon={props.icon.icon}
+          width={props.iconWidth}
+          height={props.iconHeight}
+        />
+      </Show>
+      <Text x={styles.paddingX + 50} style={styles.textStyle}>
+        {props.title}
+      </Text>
     </node>
   );
 };
