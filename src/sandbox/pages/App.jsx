08f7c10af9f6a21fcbef4692460fc4a5e51f9ef9
Anthony Gallo <anthony_gallo2@comcast.com>
2023-11-08 14:50:54 -0500

    chore: clean up sandbox/app.jsx

--- src/sandbox/pages/App.jsx
+++ src/sandbox/pages/App.jsx
@@ -16,13 +16,13 @@
  */
 
 import { View } from '@lightningjs/solid';
-import { ProgressBar } from '@lightningjs/solid-ui';
+// import {} from '@lightningjs/solid-ui';
+
 const App = () => {
   return (
     // eslint-disable-next-line solid/style-prop
     <View ref={window.APP} style={{ width: 1920, height: 1080 }}>
       <View color="#071423" />
-      <ProgressBar x={200} y={400} width={500} progress={0.6} zIndex={1} />
     </View>
   );
 };
