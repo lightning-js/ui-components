c759115d5f9a0d98144655929b0c3c482d30ee6a
Mandy Wehrman <arwehrman@gmail.com>
2023-11-17 12:35:15 -0500

    fix(Storybook): adds css overrides to Storybook

--- src/packages/solid/.storybook/main.ts
+++ src/packages/solid/.storybook/main.ts
@@ -17,13 +17,22 @@
 
 const config = {
   stories: ['../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false
+      }
+    },
+    '@storybook/addon-links',
+    '@storybook/addon-interactions'
+  ],
   framework: {
-    name: "@storybook/html-vite",
-    options: {},
+    name: '@storybook/html-vite',
+    options: {}
   },
   docs: {
     autodocs: 'tag'
-  },
+  }
 };
 export default config;
