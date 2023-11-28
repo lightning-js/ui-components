35b2fa9f794b6c486a6dcf5c5d5912713cdd1510
Anthony Gallo <anthony_gallo2@comcast.com>
2023-11-28 16:34:22 -0500

    feat: add tone state mapper util

---
+++ src/packages/solid/utils/index.js
@@ -0,0 +1,1 @@
+export { default as mapToneToStateHook } from './mapToneToStateHook';
---
+++ src/packages/solid/utils/mapToneToStateHook.ts
@@ -0,0 +1,14 @@
+import type { ElementNode } from '@lightningjs/solid';
+
+export default (node: ElementNode, states: ElementNode['States']) => {
+  const nextState = node.tone
+    ? states.map((state: string | { string: boolean }) => {
+        if (state === node.tone) {
+          return state; // don't create tone-tone states
+        } else {
+          return `${node.tone}-${state}`;
+        }
+      })
+    : states;
+  return nextState;
+};
