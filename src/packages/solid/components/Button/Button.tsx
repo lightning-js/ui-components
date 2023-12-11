452205b47289b2c6e86378c6ee49cfc03211203a
Anthony Gallo <anthony_gallo2@comcast.com>
2023-12-11 16:45:12 -0500

    chore: rework themestyle types, update button to main

--- src/packages/solid/components/Button/Button.tsx
+++ src/packages/solid/components/Button/Button.tsx
@@ -17,96 +17,26 @@
 
 import { type Component } from 'solid-js';
 import { View, Text, type NodeProps } from '@lightningjs/solid';
-import Icon, { type IconProps } from '../Icon/Icon.jsx';
-import Checkbox, { type CheckboxProps } from '../Checkbox/Checkbox.jsx';
-import styles, { type ButtonStyle } from './Button.styles.js';
-import type { Tone } from '../../../../shared/types/solid.js';
-import { withPadding } from '@lightningjs/solid-primitives';
-withPadding;
+// import styles from './Button.styles.js';
+import styles from './Button.styleLookups.js';
+import type { Tone } from 'types';
 
-/**
- * Primary UI component for user interaction
- */
-export interface ButtonProps extends ButtonStyleProps, Omit<NodeProps, 'style'> {
-  suffix?: {
-    checkbox?: Partial<CheckboxProps>;
-    icon?: Partial<IconProps>;
-  };
-  prefix?: {
-    checkbox?: Partial<CheckboxProps>;
-    icon?: Partial<IconProps>;
-  };
-  width?: number;
-  height?: number;
+export interface ButtonProps extends NodeProps {
+  children: string | string[];
   tone?: Tone;
-  style?: ButtonStyle;
 }
 
-export interface ButtonStyleProps {
-  borderRadius?: number;
-}
-
-const Button: Component<ButtonProps & { children: string | string[] }> = props => {
+const Button: Component<ButtonProps> = props => {
+  console.log(styles);
   return (
-    <node
-      use:withPadding={styles.Container?.padding}
-      animate
-      forwardStates
-      tone={props.tone || styles.tone}
-      {...{
-        ...styles.Container[props.tone || styles.tone],
-        ...props?.style?.Container,
-        ...props
-      }}
-      style={styles.Container}
-    >
-      <View
-        forwardStates
-        width={props.width}
-        tone={props.tone || styles.tone}
-        {...{
-          ...styles.FlexContainer[props.tone || styles.tone],
-          ...props?.style?.FlexContainer
-        }}
-        style={styles.FlexContainer}
-      >
-        <Show when={props.prefix?.icon}>
-          <Icon {...props.prefix?.icon} tone={props.tone || styles.tone} />
-        </Show>
-
-        <Show when={props.prefix?.checkbox}>
-          <Checkbox {...props.prefix?.checkbox} tone={props.tone || styles.tone} />
-        </Show>
-
-        <Show when={props.children} keyed>
-          {children => (
-            <Text
-              tone={props.tone || styles.tone}
-              {...{
-                ...styles.Text[props.tone || styles.tone],
-                ...props?.style?.Text
-              }}
-              style={styles.Text}
-            >
-              {children}
-            </Text>
-          )}
-        </Show>
-
-        <Show when={props.suffix?.icon}>
-          <Icon {...props.suffix?.icon} tone={props.tone || styles.tone} />
-        </Show>
-
-        <Show when={props.suffix?.checkbox}>
-          <Checkbox {...props.suffix?.checkbox} tone={props.tone || styles.tone} />
-        </Show>
-      </View>
-    </node>
+    <View {...props} style={styles.Container} tone={props.tone || styles.tone} forwardStates>
+      <Text style={styles.Text}>{props.children}</Text>
+    </View>
   );
 };
 
-export const ButtonContainer: Component<NodeProps> = props => {
-  return <View {...props} style={styles.Container} forwardStates />;
+export const ButtonContainer: Component<ButtonProps> = props => {
+  return <View {...props} style={styles.Container} tone={props.tone || styles.tone} forwardStates />;
 };
 
 export default Button;
