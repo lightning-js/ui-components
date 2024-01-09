ade0eacaa72109ac6541cc8562e964ad378b83aa
Anthony Gallo <anthony_gallo2@comcast.com>
2024-01-09 15:59:23 -0500

    checkbox: update to use latest theme

--- src/packages/solid/components/Checkbox/Checkbox.styles.ts
+++ src/packages/solid/components/Checkbox/Checkbox.styles.ts
@@ -38,17 +38,17 @@ const styles = {
     justifyContent: 'center',
     width: size,
     height: size,
-    color: getHexColor(...(theme.color.fillNeutral as [string, number])),
+    color: theme.color.fillNeutral,
     borderRadius: size / 4,
     border: {
       width: strokeWidth,
-      color: getHexColor(...(theme.color.strokeInverse as [string, number]))
+      color: theme.color.strokeInverse
     },
     disabled: {
       alpha: theme.alpha.inactive
     },
     checked: {
-      color: getHexColor(...(theme.color.fillNeutralDisabled as [string, number]))
+      color: theme.color.fillNeutralDisabled
     }
   }
 } as const;
