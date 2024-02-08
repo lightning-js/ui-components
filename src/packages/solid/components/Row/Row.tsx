ca7157370bce4e9d1169637f58f65acf3b7a74eb
Chris Lorenzo <christopher_lorenzo@comcast.com>
2024-02-08 09:39:32 -0500

    fix: chain styling with row + column (#58)

--- src/packages/solid/components/Row/Row.tsx
+++ src/packages/solid/components/Row/Row.tsx
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2024 Comcast Cable Communications Management, LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { type Component } from 'solid-js';
-import { type IntrinsicNodeProps } from '@lightningjs/solid';
+import { ElementNode, type IntrinsicNodeProps } from '@lightningjs/solid';
 import { Row as SolidRow } from '@lightningjs/solid-primitives';
 import styles from './Row.styles.js';
 import { withScrolling } from '../../utils/withScrolling.js';
@@ -8,6 +25,13 @@ import { chainFunctions } from '../../index.js';
 export interface RowProps extends IntrinsicNodeProps {
   scrollIndex?: number;
   scrollType?: 'alwaysScroll' | 'neverScroll' | 'lazyScroll';
+  onSelectedChanged?: (
+    this: ElementNode,
+    elm: ElementNode,
+    active: ElementNode,
+    selectedIndex: number,
+    lastSelectedIndex: number
+  ) => void;
 }
 
 const Row: Component<RowProps> = (props: RowProps) => {
@@ -15,7 +39,7 @@ const Row: Component<RowProps> = (props: RowProps) => {
   return (
     <SolidRow
       {...props}
-      style={styles.Container}
+      style={props.style ? [props.style, styles.Container] : styles.Container}
       onSelectedChanged={chainFunctions(props.onSelectedChanged, withScrolling(props.x as number))}
     />
   );
--- src/packages/solid/utils/chainFunctions.ts
+++ src/packages/solid/utils/chainFunctions.ts
@@ -1,4 +1,22 @@
-export const chainFunctions = (callback: Function | any, originalFunc: Function) => {
+/*
+ * Copyright 2024 Comcast Cable Communications Management, LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* eslint-disable @typescript-eslint/ban-types */
+export const chainFunctions = (callback: Function | undefined, originalFunc: Function) => {
   return (...args: any[]) => {
     typeof callback === 'function' && callback.apply(this, args);
     originalFunc.apply(this, args);
