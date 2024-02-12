673ea1403bed7e2cf8bef954e81cfcd6610c506d
Anthony Gallo <anthony_gallo2@comcast.com>
2024-02-12 12:12:05 -0500

    fix(utils): update stateMapperHook type to only work with states as arrays of strings

--- src/packages/solid/utils/mapToneToStateHook.ts
+++ src/packages/solid/utils/mapToneToStateHook.ts
@@ -1,9 +1,9 @@
 import type { ElementNode } from '@lightningjs/solid';
 
 // TODO update states to use ElementNode['States'] when support is added
-export default (node: ElementNode, states: { string: boolean }[]) => {
+export default (node: ElementNode, states: string[]) => {
   const nextState = node.tone
-    ? states.map((state: string | { string: boolean }) => {
+    ? states.map((state: string) => {
         if (state === node.tone) {
           return state; // don't create tone-tone states
         } else {
