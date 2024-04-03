9dc0a8f616dc041314743c71f9bc7ddaee912d5b
Shayna Hay <shaynahay2023@gmail.com>
2024-04-03 13:50:31 -0400

    fix(Utils): adds license header and removes theme as a parameter from getDimensions and getItemRatioDimensions

--- src/packages/solid/utils/getDimensions.ts
+++ src/packages/solid/utils/getDimensions.ts
@@ -1,6 +1,24 @@
-import { getItemRatioDimensions } from "./getItemRatioDimensions.js";
+/*
+ * Copyright 2024 Comcast Cable Communications Management, LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-export function getDimensions(theme, obj = {}, fallback = {}) {
+import { getItemRatioDimensions } from './getItemRatioDimensions.js';
+import theme from 'theme';
+
+export function getDimensions(obj = {}, fallback = {}) {
   const { w, h, ratioX, ratioY, upCount } = obj;
   const fallbackW = fallback.w || 0;
   const fallbackH = fallback.h || 0;
