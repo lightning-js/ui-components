8ce974e4da3dbe9e2d793c62d150e60d78ed7f21
Anthony Gallo <anthony9187@users.noreply.github.com>
2024-04-16 13:27:21 -0400

    feat(ListItem): add Container

--- src/packages/solid/components/ListItem/ListItem.tsx
+++ src/packages/solid/components/ListItem/ListItem.tsx
@@ -1,30 +1,48 @@
-import { type Component } from 'solid-js';
-import { Text, View } from '@lightningjs/solid';
-import styles, { type ListItemStyles } from './ListItem.styles.js';
-import type { Tone } from '../../types/types.js';
+/*
+ * Copyright 2024 Comcast Cable Communications Management, LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-export interface ListItemProps {
-  title?: string,
-  description?: string,
-  tone?: Tone;
-  style?: Partial<ListItemStyles>;
+import { type Component } from 'solid-js';
+import { Text, View, type NodeProps } from '@lightningjs/solid';
+import type { UIComponentProps } from '../../types/interfaces.js';
+import styles from './ListItem.styles.js';
+
+export interface ListItemProps extends UIComponentProps {
+  title?: string;
+  description?: string;
 }
 
-const ListItem: Component<ListItemProps> = (props) => {
+export interface ListItemContainerProps extends UIComponentProps {
+  children?: NodeProps['children'];
+}
+
+const ListItem: Component<ListItemProps> = props => {
   return (
-    <View
+    <ListItemContainer
       {...props}
       style={[
-        ...[props.style].flat(),
-        styles.Container.tones?.[props.tone ?? styles.tone], // Assuming defaultTone is defined somewhere or use a literal string
+        ...props.style, //
+        styles.Container.tones?.[props.tone ?? styles.tone],
         styles.Container.base
       ]}
       forwardStates
     >
       <Text
         style={[
-          ...[props.style?.Title].flat(),
-          styles.Title.tones[props.tone ?? styles.tone], // Same assumption as above
+          styles.Title.tones[props.tone ?? styles.tone], //
           styles.Title.base
         ]}
       >
@@ -33,15 +51,33 @@ const ListItem: Component<ListItemProps> = (props) => {
       <Text
         y={styles.Description.base.y}
         style={[
-          ...[props.style?.Description].flat(),
-          styles.Description.tones[props.tone ?? styles.tone], // Same assumption as above
+          styles.Description.tones[props.tone ?? styles.tone], //
           styles.Description.base
         ]}
       >
         {props.description}
       </Text>
-    </View>
+    </ListItemContainer>
   );
-}
+};
 
-export default ListItem;
+const ListItemContainer: Component<ListItemContainerProps> = props => {
+  return (
+    <View
+      {...props}
+      style={[
+        props.style, //
+        styles.Container.tones?.[props.tone ?? styles.tone],
+        styles.Container.base
+      ]}
+      forwardStates
+    />
+  );
+};
+
+export {
+  ListItem as default, //
+  ListItemContainer,
+  ListItemProps,
+  ListItemContainerProps
+};
