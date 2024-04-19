159f0bf761b90937833c6d4e80e1d301f157a26e
David Richards, Jr <dbarichardsjr@gmail.com>
2024-04-19 15:04:08 -0400

    feat(Storybook): initial install

---
+++ src/packages/blits/.storybook/main.js
@@ -0,0 +1,27 @@
+/** @type { import('@storybook/html-vite').StorybookConfig } */
+const config = {
+  // stories: ['../components/**/*.stories.@(js|jsx|mjs|ts|tsx)', '../docs/*.mdx'],
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false, // disable background addon
+        outline: false, // disable outline addon
+        measure: false, // disable measure addon
+        viewport: false // disable viewport addon
+      }
+    },
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions'
+  ],
+  framework: {
+    name: '@storybook/html-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  }
+};
+export default config;
