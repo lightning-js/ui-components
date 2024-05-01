2bf85942f319cb450e40277457a29f093bab2192
Matt Wooding <44930483+WoodingMP@users.noreply.github.com>
2024-05-01 09:05:16 +0100

    chore: swap timeout for mutationObserver in preview

--- src/packages/blits/.storybook/preview.ts
+++ src/packages/blits/.storybook/preview.ts
@@ -1,30 +1,40 @@
 import Blits from '@lightningjs/blits';
 import type { Preview } from '@storybook/html';
 
-
 const launchApp = ({name, fn, template}) => {
 
-// app with single component
-const App = Blits.Application({
-  components: {
-    [name]: fn
-  },
-  template,
-})
-
-// timeout to wait until app div is available
-setTimeout(() => {
-  Blits.Launch(App, 'app', {
-    w: 1920,
-    h: 1080,
-    multithreaded: false,
-    debugLevel: 1,
-    inspector: true
+  // app with single component
+  const App = Blits.Application({
+    components: {
+      [name]: fn
+    },
+    template,
   })
-}, 0)
+
+  // observer waits until app element is found before launching blits app
+  const observer = new MutationObserver((mutationList, observer) => {
+    const appElement = document.getElementById('app');
+
+    if (appElement) {
+      observer.disconnect();
+
+      Blits.Launch(App, 'app', {
+        w: 1920,
+        h: 1080,
+        multithreaded: false,
+        debugLevel: 1,
+        inspector: true
+      })
+    }
+  });
+
+  // start observing root until app element is initialised
+  observer.observe(document, {
+    subtree: true,
+    childList: true
+  });
 
 }
-
 
 const preview: Preview = {
   parameters: {
