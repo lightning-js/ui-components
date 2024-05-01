efb3ae69ca53f9eda4b53da4f4c4a71796302779
Anthony Gallo <anthony9187@users.noreply.github.com>
2024-05-01 15:57:54 -0400

    fix(CardContent): update to work with new Artwork setup, clean up story file

--- src/packages/solid/components/CardContent/CardContent.stories.tsx
+++ src/packages/solid/components/CardContent/CardContent.stories.tsx
@@ -22,12 +22,12 @@ import Badge from '../Badge/Badge.jsx';
 import Label from '../Label/Label.jsx';
 import Metadata, { type MetadataProps } from '../Metadata/Metadata.jsx';
 import Details, { type DetailsProps } from '../Metadata/Details.jsx';
+import { hexColor } from '@lightningjs/solid';
 
 type Story = StoryObj<typeof CardContent>;
 /** cspell: disable */
 const lorum =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sodales est eu eleifend interdum. Vivamus egestas maximus elementum. Sed condimentum ligula justo, non sollicitudin lectus rutrum vel. Integer iaculis vitae nisl quis tincidunt. Sed quis dui vehicula, vehicula felis a, tempor leo. Fusce tincidunt, ante eget pretium efficitur, libero elit volutpat quam, sit amet porta tortor odio non ligula. Ut sed dolor eleifend massa auctor porttitor eget ut lectus. Vivamus elementum lorem mauris, eu luctus tortor posuere sit amet. Nunc a interdum metus.';
-/** cspell: enable */
 
 const meta: Meta<typeof CardContent> = {
   title: 'Components/CardContent',
@@ -109,8 +109,7 @@ export const Basic: Story = {
         linearGradient: {
           angle: 3.14,
           stops: [0, 0.5],
-          // @ts-expect-error colors can be strings but type expects numbers
-          colors: [theme.color.black, 0x00000000]
+          colors: [hexColor(theme.color.black), 0x00000000]
         }
       }
     },
